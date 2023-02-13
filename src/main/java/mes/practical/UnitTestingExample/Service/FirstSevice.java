c3500466ef3a7194928943abed185f5dd630d418
mahadevv-at-719945103823 <mahadevv-at-719945103823>
2023-02-13 10:46:22 +0530

    Java Doc

--- src/main/java/mes/practical/UnitTestingExample/Service/FirstSevice.java
+++ src/main/java/mes/practical/UnitTestingExample/Service/FirstSevice.java
@@ -2,14 +2,17 @@ package mes.practical.UnitTestingExample.Service;
 
 import org.springframework.stereotype.Service;
 
-@Service 
-//Annotation Denoting a service. 
-//Service A place where code control in done. 
-//Most of the business logic comes here.
+@Service
 public class FirstSevice {
-
-    public int addTwoNumbers(int number1, int number2){
-        return number1+number2;
+    /**
+     * Function to add two number
+     * 
+     * @param number1 - Number 1
+     * @param number2 - NUmber 2
+     * @return sum of two Number 1 and Number 2
+     */
+    public int addTwoNumbers(int number1, int number2) {
+        return number1 + number2;
     }
-    
+
 }
--- src/test/java/mes/practical/UnitTestingExample/UnitTestingExampleApplicationTests.java
+++ src/test/java/mes/practical/UnitTestingExample/UnitTestingExampleApplicationTests.java
@@ -3,7 +3,6 @@ package mes.practical.UnitTestingExample;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import mes.practical.UnitTestingExample.Service.FirstSevice;
